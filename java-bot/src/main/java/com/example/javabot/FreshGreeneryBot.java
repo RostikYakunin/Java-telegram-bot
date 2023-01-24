36ee3bbe957ce2bfb4c7f8607bb3b8e227357e3b
Rostyslav Yakunin <yakunin.dev.official@gmail.com>
2023-01-24 14:54:03 +0200

    final

--- java-bot/src/main/java/com/example/javabot/FreshGreeneryBot.java
+++ java-bot/src/main/java/com/example/javabot/FreshGreeneryBot.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FreshGreeneryBot {
-
     public static void main(String[] args) {
         SpringApplication.run(FreshGreeneryBot.class, args);
         System.out.println("Start Bot !");
     }
-
 }
